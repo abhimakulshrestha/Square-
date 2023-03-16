931429ab8a30b0d290d45d91df5ef93831905bd8
abhimakulshrestha <abhimakulshrestha2004@gmail.com>
2023-03-16 11:06:57 +0530

    string

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,18 @@ public class Main{
 
 
     public static void main(String[] args) {
-        String name = "Abhima";
-        name = name.toLowerCase();
-        System.out.println(name);
+        //String name = "Abhima";
+        //name = name.toLowerCase();
+       // System.out.println(name);
 
 
-        String text =" How are     you";
-        text = text.replace( ' ','_');
-        System.out.println(text);
+      //  String text =" How are     you";
+      //  text = text.replace( ' ','_');
+      //  System.out.println(text);
 
+        String letter = " Dear <|name|>, Thanks a lot";
+        letter = letter.replace("<|name|>","ABHIMA");
+        System.out.println(letter);
 
     }
 }
