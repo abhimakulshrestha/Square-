e557b23e79f0826ecfa81b0d4b4f7919f14da62f
abhimakulshrestha <abhimakulshrestha2004@gmail.com>
2023-03-16 10:37:05 +0530

    string

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main{
+
+
+    public static void main(String[] args) {
+        String name = "Abhima";
+        name = name.toLowerCase();
+        System.out.println(name);
+
+
+    }
+}
---
+++ src/java.java
@@ -0,0 +1,12 @@
+public class java{
+
+    public static void Java(String[] args) {
+        //write your code here
+        System.out.println("The sum of these number is : ");
+        int num1=6;
+        int num2=5;
+        int num3=7;
+        int sum = num1 +num2 +num3;
+        System.out.println(sum);
+    }
+}
