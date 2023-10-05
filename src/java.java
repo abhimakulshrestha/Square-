a8856a1461c2bae5c999f2f55b7da4a79ce19dfb
abhimakulshrestha <abhimakulshrestha2004@gmail.com>
2023-10-05 20:51:04 +0530

    string

--- src/java.java
+++ src/java.java
@@ -1,12 +1,6 @@
-public class java{
+public class java {
 
-    public static void Java(String[] args) {
-        //write your code here
-        System.out.println("The sum of these number is : ");
-        int num1=6;
-        int num2=5;
-        int num3=7;
-        int sum = num1 +num2 +num3;
-        System.out.println(sum);
+    public static void Main(String[] args) {
+        System.out.println("hello world");
     }
 }
