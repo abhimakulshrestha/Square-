ad7a15e711eb64d25cce18eebdced55e8ec06d51
abhimakulshrestha <abhimakulshrestha2004@gmail.com>
2023-10-05 21:09:53 +0530

    Largest number

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,41 +1,24 @@
 package com.company;
 import java.util.Scanner;
- class Square {
+public class Main{
+public static void main(String[]args){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter value for a : ");
+    int a = sc.nextInt();
+    System.out.print("Enter value for b : ");
+    int b = sc.nextInt();
+    System.out.print("Enter value for c : ");
+    int c = sc.nextInt();
 
-         Scanner sc = new Scanner(System.in);
-         int a = sc.nextInt();
+    int max = a;
 
+    if (b > max){
+        max= b;
+    }
+    if (c > max){
+        max = c ;
+    }
+    System.out.println("Max = "+ max);
+}
 
-     public void getSides() {
-         System.out.println("length of sides of square = " +a+ " cm");
-
-     }
-
-     public int getArea() {
-         int Area = a * a;
-         System.out.println("Area of square = "+Area+" cm sq");
-      return Area;
-     }
-
-     public int getPerimeter() {
-         int Perimeter = 4 * a;
-         System.out.println("Perimeter of square = "+Perimeter+ " cm");
-         return Perimeter;
-     }
-
-     public static void main(String[] args) {
-         Square Square1 = new Square();
-
-         Square1.getSides();
-         Square1.getArea();
-         Square1.getPerimeter();
-     }
- }
-
-
-
-
-
-
-
-
+        }
